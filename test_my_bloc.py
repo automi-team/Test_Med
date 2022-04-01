57989217ca12162e0c02555d46ee38ec6ac30a52
MhdKAT <mohamed@automi.ai>
2022-04-01 14:38:24 +0200

    test

--- test_my_bloc.py
+++ test_my_bloc.py
@@ -1,8 +1,11 @@
 import my_bloc
 class Testbloc:
+    """ test bench for my_bloc """
 
     def test_5(self):
+        """ simple test """
         assert 25 == my_bloc.square(5)
 
     def test_10(self):
+        """ simple test """
         assert 100 == my_bloc.square(10)
