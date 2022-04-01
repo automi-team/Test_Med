7170013754a5e315642aeb41a6f77f21e0896a19
MhdKAT <mohamed@automi.ai>
2022-04-01 15:01:25 +0200

    test

--- test_my_bloc.py
+++ test_my_bloc.py
@@ -1,11 +1,9 @@
 import my_bloc
 class Testbloc:
     """ test bench for my_bloc """
-    @staticmethod
     def test_5(self):
         """ simple test """
         assert 25 == my_bloc.square(5)
-    @staticmethod
     def test_10(self):
         """ simple test """
         assert 100 == my_bloc.square(10)
